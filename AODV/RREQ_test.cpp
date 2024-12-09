bcc4457c368770c467b90b7cdc228d1e5dc31618
Ukyo <e1n21077@oit.ac.jp>
2024-12-09 13:27:39 +0000

    RREQ

---
+++ AODV/RREQ_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include "data_struct.h"
+#include <random>
+
+int main() {
+    uint32_t origin_ip = 0xC0A80101; // 192.168.1.1
+    uint32_t dest_ip = 0xC0A80102;   // 192.168.1.2
+    uint32_t rreq_id = 100;
+    uint32_t origin_seq_num = 42;
+    uint8_t hop_count = 0;
+    uint8_t flags = 10;
+
+    RREQ rreq = generateRREQ(flags,hop_count,origin_ip,dest_ip,origin_seq_num,rreq_id);
+
+    // 結果を出力
+    std::cout << "Generated RREQ:" << std::endl;
+    std::cout << "Type: " << static_cast<int>(rreq.type) << std::endl;
+    std::cout << "Flags: " << static_cast<int>(rreq.flags) << std::endl;
+    std::cout << "Hop Count: " << static_cast<int>(rreq.hop_count) << std::endl;
+    std::cout << "RREQ ID: " << rreq.rreq_id << std::endl;
+    std::cout << "Destination IP: " << rreq.dest_ip << std::endl;
+    std::cout << "Destination Seq Num: " << rreq.dest_seq_num << std::endl;
+    std::cout << "Originator IP: " << rreq.origin_ip << std::endl;
+    std::cout << "Originator Seq Num: " << rreq.origin_seq_num << std::endl;
+
+    return 0;
+}
--- AODV/data_struct.h
+++ AODV/data_struct.h
@@ -4,14 +4,14 @@
 
 //RREQの雛形
 struct RREQ {
-    uint8_t type = 1; // RREQタイプ
-    uint8_t flags = 0;
-    uint8_t hop_count = 0;
-    uint32_t rreq_id;
-    uint32_t dest_ip;
-    uint32_t dest_seq_num;
-    uint32_t origin_ip;
-    uint32_t origin_seq_num;
+    uint8_t type; // RREQタイプ
+    uint8_t flags; //J,R,G,D,Uのどれかが設定される。それぞれの意味はrfcを要参照
+    uint8_t hop_count; //送信元から経由ノードのホップカウント数
+    uint32_t rreq_id; //送信元ノードに関連づけられたときに特定のRREQを識別する一意のID   
+    uint32_t dest_ip; //宛先ノードのIPアドレス
+    uint32_t dest_seq_num; //宛先へ向かう経路について送信元が受信した最新のシーケンス番号
+    uint32_t origin_ip; //送信元ノードのIPアドレス
+    uint32_t origin_seq_num; //送信元ノードを指すルートエントリで使われるシーケンス番号
 };
 //RREPの雛形
 struct RREP {
@@ -32,12 +32,15 @@ struct RERR {
     std::vector<uint32_t> unreachable_dest_seq_nums;
 };
 //RREQの生成
-RREQ generateRREQ(uint32_t origin_ip, uint32_t dest_ip, uint32_t origin_seq_num, uint32_t rreq_id) {
+RREQ generateRREQ(uint8_t flags,uint8_t hop_count, uint32_t origin_ip, uint32_t dest_ip, uint32_t origin_seq_num, uint32_t rreq_id) {
     RREQ rreq;
-    rreq.origin_ip = origin_ip;
-    rreq.dest_ip = dest_ip;
-    rreq.origin_seq_num = origin_seq_num;
-    rreq.rreq_id = rreq_id;
+    rreq.type=1; //typeの設定
+    rreq.flags=flags; //フラグの設定
+    rreq.hop_count= hop_count; //ホップカウントの設定
+    rreq.origin_ip = origin_ip; //送信元のIPアドレスの設定
+    rreq.dest_ip = dest_ip; //宛先のIPアドレスの設定
+    rreq.origin_seq_num = origin_seq_num; //送信元ノードを指すルートエントリで使われるシーケンス番号の設定
+    rreq.rreq_id = rreq_id; //送信元ノードに関連づけられたときに特定のRREQを識別する一意のIDの設定
     return rreq;
 }
 //RREPの生成
