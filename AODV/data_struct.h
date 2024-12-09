eebc25c1ce597d7adec134a93869d5cf59174670
Ukyo <e1n21077@oit.ac.jp>
2024-12-09 10:38:47 +0000

    data_struct_update

--- AODV/data_struct.h
+++ AODV/data_struct.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <stdint.h>
 
-
+//RREQの雛形
 struct RREQ {
     uint8_t type = 1; // RREQタイプ
     uint8_t flags = 0;
@@ -13,7 +13,7 @@ struct RREQ {
     uint32_t origin_ip;
     uint32_t origin_seq_num;
 };
-
+//RREPの雛形
 struct RREP {
     uint8_t type = 2; // RREPタイプ
     uint8_t flags = 0;
@@ -23,7 +23,7 @@ struct RREP {
     uint32_t origin_ip;
     uint32_t lifetime;
 };
-
+//RERRの雛形
 struct RERR {
     uint8_t type = 3; // RERRタイプ
     uint8_t flags = 0;
@@ -31,3 +31,22 @@ struct RERR {
     std::vector<uint32_t> unreachable_dest_ips;
     std::vector<uint32_t> unreachable_dest_seq_nums;
 };
+//RREQの生成
+RREQ generateRREQ(uint32_t origin_ip, uint32_t dest_ip, uint32_t origin_seq_num, uint32_t rreq_id) {
+    RREQ rreq;
+    rreq.origin_ip = origin_ip;
+    rreq.dest_ip = dest_ip;
+    rreq.origin_seq_num = origin_seq_num;
+    rreq.rreq_id = rreq_id;
+    return rreq;
+}
+//RREPの生成
+RREP generateRREP(uint32_t origin_ip, uint32_t dest_ip, uint32_t dest_seq_num, uint8_t hop_count, uint64_t lifetime) {
+    RREP rrep;
+    rrep.origin_ip = origin_ip;
+    rrep.dest_ip = dest_ip;
+    rrep.dest_seq_num = dest_seq_num;
+    rrep.hop_count = hop_count;
+    rrep.lifetime = lifetime;
+    return rrep;
+}
